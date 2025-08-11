e8ec9ffb0acd24482d5f746af1088126dd884719
Sanyum Manhas <msanyum@gmail.com>
2025-08-11 13:42:17 +0530

    configured socket.io and Server on same Port

--- server.js
+++ server.js
@@ -3,17 +3,24 @@ const app = express();
 const mongoose = require('mongoose')
 require('dotenv').config(); 
 const port = process.env.PORT || 9000;
+const {Server} = require('socket.io');
+const http = require("http");
+
 
 const cookieParser = require('cookie-parser');
 
 const cors = require('cors');
 
-const io = require('socket.io')(8080, {
-    cors:{
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+   cors:{
         origin: "http://localhost:3000",
         credentials:true
-    }
+    }  
 })
+
 
 app.use(cors({
     origin:"http://localhost:3000",
@@ -79,7 +86,7 @@ app.use(userRoutes);
 app.use(connectionRoutes);
 app.use(msgRoutes);
 
-app.listen(port, ()=>{
+server.listen(port, ()=>{
     console.log("Server running on Port " + port)
 })
 
